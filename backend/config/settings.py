e4bbf7912b9e38f3a52400a11a623dfc0f4cb964
plmdsmatheus <matheusduartenatal@gmail.com>
2025-05-04 13:11:54 -0300

    Feat: CORS Config

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     'taggit',
     'django_filters',
+    'corsheaders',
 
     # local apps
     'apps.users',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -177,3 +179,9 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 CELERY_TASK_ALWAYS_EAGER = 'test' in sys.argv
 CELERY_TASK_EAGER_PROPAGATES = True
+
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
